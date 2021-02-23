7ca932b2145816d3beb63ccdfda9a6eb62844781
UtkarshVarma <46875773+UvRoxx@users.noreply.github.com>
2021-02-23 03:57:04 -0500

    Update main.py
    updating main to transition from sqlite to Postgres.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask_ckeditor import CKEditor
 from datetime import date
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
-
+import os
 from sqlalchemy.orm import relationship
 
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
@@ -13,12 +13,12 @@ from forms import CreatePostForm, CreateUserForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
